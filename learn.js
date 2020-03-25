0faaa1a14a5203a82fc38d3e9d366432305d03b3
MicroClone <43690001+qolbinnadhzifi@users.noreply.github.com>
2020-03-25 23:14:27 +0700

    Update learn.js

--- learn.js
+++ learn.js
@@ -1,4 +1,5 @@
 // build simple program CLI (Command Line Interface) using node.js
+const moment = require('moment');
 const http = require('http');
 const fs = require('fs');
 const hostname = '127.0.0.1';
@@ -15,8 +16,9 @@ const server = http.createServer((req,res,rej,err) =>{
 });
 
 // nothing todo
-let banner = "###########################";
+let banner = "_________________________";
 let a = " : ";
+let newLine = "\n";
 
 //color
 let color1 = "\033[30;1m";
@@ -37,3 +39,23 @@ let color15 = "\033[44;1m";
 let color16 = "\033[45;1m";
 let color17 = "\033[46;1m";
 let color18 = "\033[47;1m";
+
+//date | toLocaleDateString();
+const date = new Date();
+const years = date.getFullYear();
+const arrMonth = ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
+const month = date.getMonth();
+const arrDay = ["Senin","Selasa","Rabu","Kamis","Jum\'at","Sabtu","Ahad"];
+const day = date.getDay();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const miliseconds = date.getMilliseconds();
+const tanggal = date.getDate();
+
+console.clear();
+console.log(banner);
+console.log("Simple Program Application");
+console.log(banner);
+console.log('\n\n');
+console.log(tanggal+" "+month[arrMonth]+" "+years);
